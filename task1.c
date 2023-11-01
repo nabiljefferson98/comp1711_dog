1cb68f4e4ce499ec171c73f418dc89616b903194
Muhammad Nabil Bin Muhammad Saiful Wong <146673341+nabiljefferson98@users.noreply.github.com>
2023-11-01 17:34:03 +0000

    task 1 update

--- task1.c
+++ task1.c
@@ -11,11 +11,19 @@ typedef struct {
 
 // Define any additional variables here
 
+//helper function tu utk splitkan data dlm csv kpd date, time and steps
+//dptkan output mengikut each data (filename.date)
+//tambah array[]
+//determin how many lines / data inside the csv...maybe use for loop
+//get output how many numbers of record inside the file...while loop prints every line
+//understand 
 
 
 // This is your helper function. Do not change it in any way.
 // Inputs: character array representing a row; the delimiter character
 // Ouputs: date character array; time character array; steps character array
+
+//**Self note: tokeniseRecord split input data using delimiter between date, time and steps
 void tokeniseRecord(const char *input, const char *delimiter,
                     char *date, char *time, char *steps) {
     // Create a copy of the input string as strtok modifies the string
@@ -44,5 +52,40 @@ void tokeniseRecord(const char *input, const char *delimiter,
 // Complete the main function
 int main() {
 
+    char* filename = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    // Value to hold per line at the time in the FitnessData
+    char buffer[255];
+
+    // Create a suitable size and structure array
+    // Store up to 255 Fitness Data records from the file into that array
+    FITNESS_DATA fitness_data[255];
+
+    // To store number of value read successfully from each line 
+    //of the file, so that we can verify that each line was read in successfully
+    int read = 0;
+
+    // To track the number of records in the file
+    int record = 0;
+
+    do{
+        // returns the number of values that it was able to read in successfully
+        read = fscanf(file,
+                    "%c,%c,%d\n",
+                    &fitness_data[record].date,
+                    &fitness_data[record].time,
+                    &fitness_data[record].steps);
+
+
+    }while (!feof(file)); 
+    // ! feof means continue so long as we haven't reached the end of the file
+
+    fclose(file);
+    return 0;
 
 }
--- week4_c_bootcamp/read_from_file.c
+++ week4_c_bootcamp/read_from_file.c
@@ -8,7 +8,8 @@ int main() {
         return 1;
     }
 
-    int buffer_size = 100;
+    //bwh ni declare array size 100
+    int buffer_size = 100; 
     char line_buffer[buffer_size];
 
     while (fgets(line_buffer, buffer_size, file) != NULL) {
@@ -18,3 +19,54 @@ int main() {
     fclose(file);
     return 0;
 }
+
+/*
+
+        // Open the CSV file for reading
+    FILE *file = fopen("FitnessData_2023.csv", "r");
+    if (file == NULL) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    int MAX_RECORDS = 100; // Define the maximum number of records
+    FITNESS_DATA fitnessData[MAX_RECORDS]; // Declare an array of FITNESS_DATA structures
+    
+    // Define the delimiter character
+    const char *delimiter = ",";
+
+    int numRecords = 0;
+    char line_buffer[255];
+
+    // Read and process the first three rows from the CSV file
+    for (int i = 0; i < 3; ++i) {
+        // Read a line from the file
+        if (fgets(line_buffer, sizeof(line_buffer), file) != NULL) {
+            // Tokenize the line and store data in the FITNESS_DATA structure
+            char date[11], time[6], steps[100];
+            tokeniseRecord(line_buffer, delimiter, date, time, steps);
+
+            // Store tokenized data in the FITNESS_DATA structure
+            strcpy(fitnessData[numRecords].date, date);
+            strcpy(fitnessData[numRecords].time, time);
+            fitnessData[numRecords].steps = atoi(steps);
+
+            // Increment the number of records
+            numRecords++;
+
+            // Print the tokenized data for this row
+            printf("Row %d: Date: %s, Time: %s, Steps: %d\n", i + 1, date, time, fitnessData[i].steps);
+        } else {
+            // If there are no more lines in the file, break the loop
+            break;
+        }
+    }
+
+    // Close the file
+    fclose(file);
+
+    return 0;
+}
+
+
+*/
